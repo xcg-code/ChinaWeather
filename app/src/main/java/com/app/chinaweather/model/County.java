6b40fb6dedb27edb51d10cede9477c543fed4bab
xcg-code <1450152983@qq.com>
2016-05-24 22:42:18 +0800

    新增数据库帮助类，以及表的实体类

---
+++ app/src/main/java/com/app/chinaweather/model/County.java
@@ -0,0 +1,43 @@
+package com.app.chinaweather.model;
+
+/**
+ * Created by 14501_000 on 2016/5/24.
+ */
+public class County {
+    private int id;
+    private String countyName;
+    private String countyCode;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
---
+++ app/src/main/java/com/app/chinaweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.app.chinaweather.model;
+
+/**
+ * Created by 14501_000 on 2016/5/24.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
